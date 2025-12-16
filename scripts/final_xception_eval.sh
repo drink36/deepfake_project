73bd7f6629f9a6ea598bef967eb862983c78276c
drink36 <ooo910809@gmail.com>
2025-12-16 04:15:28 -0500

    feat: update new scripysd

--- scripts/final_xception_eval.sh
+++ scripts/final_xception_eval.sh
@@ -9,4 +9,4 @@
 #SBATCH --ntasks-per-node=1
 
 conda activate cv_env
-python models/infer.py --data_root /fs/scratch/PAS3162/drink36/AV-Deepfake1M-PlusPlus --checkpoint ./ckpt/xception_20251214_224848/last.ckpt --model xception --metadata_txt testB_files.txt --subset testB --batch_size 1024 --gpus 1
+python models/infer.py --data_root /fs/scratch/PAS3162/drink36/AV-Deepfake1M-PlusPlus --checkpoint ./ckpt/xception_20251215_202308/last.ckpt --model xception --metadata_txt testB_files.txt --subset testB --batch_size 1024 --gpus 1
